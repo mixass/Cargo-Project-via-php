4e0d8123dc1fb00f023dd2af8c631f03b66f2ef9
mixass <67761405+mixass@users.noreply.github.com>
2021-06-28 09:00:25 +0300

    Update create-customer.php

--- create-customer.php
+++ create-customer.php
@@ -12,7 +12,7 @@ function database(){
     $namesurname = $_POST["name-surname"];
     $phone = $_POST["phone"];
     $adress = $_POST["address"];
-    $trackingno = $_POST["tracking-no"];
+    $trackingno = rand(1000,99999);
     $staff = $_POST["staff"];
     $ip = $_SERVER['REMOTE_ADDR'];
     $time = date("d/m/Y");
@@ -58,7 +58,6 @@ function warn($a) {
                 Customer Name-Surname <br> <input class="inp" type="text" name = "name-surname" autocomplete="off" required="on"><br><br>
                 Phone <br> <input class="inp" type="number" name = "phone" autocomplete="off" required="on"><br><br>
                 Address <br> <textarea class="inp" name = "address" autocomplete="off" required="on" rows="10" cols="20"></textarea><br><br>
-                Tracking No <br> <input class="inp" type="number" name = "tracking-no" autocomplete="off" required="on"><br><br>
                 Staff <br> <input class="inp" type="text" name = "staff" autocomplete="off" required="on"><br><br>
             </div>
             <button class="btn btn-outline-light" type= "submit"> Create</button>
